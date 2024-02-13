55d982494eab7371d1d6227aaa515af8dcae5563
Veljko03 <veljkodovragovic@gmail.com>
2024-02-13 19:57:46 +0100

    finishing game

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ let options = ["Rock", "Paper","Scissors"];
 function getComputerChoice(){
     let choice = options[Math.floor(Math.random() * options.length)];
     choice = choice.toLowerCase();
-    console.log(choice);
+    console.log(choose);
 
     return choice;
 }
